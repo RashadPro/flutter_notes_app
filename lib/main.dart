403b4bd20fb110184f1af26d05aca5895ff8b51a
Mohamed Rashad <engineer.rachad@gmail.com>
2024-10-16 22:31:42 +0300

    Last Changes

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:notes_app/constants.dart';
-import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/simple_bloc_observer.dart';
-
 import 'package:notes_app/views/notes_view.dart';
-
 import 'cubits/notes_cubit/notes_cubit.dart';
 
 void main() async {
@@ -26,7 +23,7 @@ class NotesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-       create: (context) => NotesCubit(),
+      create: (context) => NotesCubit(),
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Poppins'),
