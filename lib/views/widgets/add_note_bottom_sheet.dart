dde2a2fd03c82eaa694dda8fcb4285c2caf9cfce
Mohamed Rashad <engineer.rachad@gmail.com>
2024-10-16 20:49:58 +0300

    create notes state

--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:notes_app/cubit/add_note_cubit/add_note_cubit.dart';
 import 'add_note_form.dart';
 
